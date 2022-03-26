de64ba58816d994db928496783a038af9d0aa244
최은석 <trgf456@naver.com>
2022-03-26 19:54:17 +0900

    3/26 리덕스 소켓 추가

---
+++ AppInner.tsx
@@ -0,0 +1,59 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React, {useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import useSocket from './src/hooks/useSocket';
+import Delivery from './src/pages/Delivery';
+import Orders from './src/pages/Orders';
+import Settings from './src/pages/Settings';
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
+import {RootState} from './src/store/reducer';
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function AppInner() {
+  const [socket, disconnect] = useSocket();
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  useEffect(() => {
+    const helloCallback = (data: any) => {
+      console.log(data);
+    };
+    if (socket && isLoggedIn) {
+      console.log(socket);
+      socket.emit('login', 'hello');
+      socket.on('hello', helloCallback);
+    }
+    return () => {
+      if (socket) {
+        socket.off('hello', helloCallback);
+      }
+    };
+  }, [isLoggedIn, socket]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      console.log('!isLoggedIn', !isLoggedIn);
+      disconnect();
+    }
+  }, [isLoggedIn, disconnect]);
+  return (
+    <NavigationContainer>
+      {isLoggedIn ? (
+        <Tab.Navigator>
+          <Tab.Screen name="Orders" component={Orders} options={{title: '오더 목록'}} />
+          <Tab.Screen name="Delivery" component={Delivery} options={{headerShown: false}} />
+          <Tab.Screen name="Settings" component={Settings} options={{title: '내 정보'}} />
+        </Tab.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="SignIn" component={SignIn} options={{title: '로그인'}} />
+          <Stack.Screen name="SignUp" component={SignUp} options={{title: '회원가입'}} />
+        </Stack.Navigator>
+      )}
+    </NavigationContainer>
+  );
+}
+export default AppInner;
